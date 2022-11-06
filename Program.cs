c0fa128bd48aa271af5a8725f30aea41c04531f0
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 01:08:13 +0530

    Add files via upload
    Adding UC1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC1 obj = new UC1();
+            obj.Validate();
+        }
+    }
+
+}
---
+++ UC1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    internal class UC1
+    {
+        public string Firstdigit = "^[A-Z]{1}[a-z]{2}$";
+        public string Firstdigit1 = "Raj";
+        public void Validate()
+        {
+            Func<string, bool> result = (Firstdigit1) => Regex.IsMatch(Firstdigit1, Firstdigit);
+            Console.WriteLine(result(Firstdigit1));
+        }
+    }
+}
