ceed4b6c8579fa448ed934f72ec706d0100d76b1
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:21:40 +0530

    Add files via upload
    Adding UC8

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC8SpecialChar obj = new UC8SpecialChar();
+            obj.Validate();
+
+        }
+    }
+}
---
+++ UC8SpecialChar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8
+{
+    internal class UC8SpecialChar
+    {
+        public string password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        public string password1 = "AparnaMishra@123S";
+        public void Validate()
+        {
+            Func<string, bool> result = (password1) => Regex.IsMatch(password1, password);
+            Console.WriteLine(result(password1));
+        }
+    }
+}
