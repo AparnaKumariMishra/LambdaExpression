77d4697ccf414b7ec5453b50a9af0fc29fc7c568
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:17:57 +0530

    Add files via upload
    Adding UC5

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC5
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC5Min8Char obj = new UC5Min8Char();
+            obj.Validate();
+        }
+    }
+}
---
+++ UC5Min8Char.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC5
+{
+    internal class UC5Min8Char
+    {
+        public string password = "^[a-z]{8,}$";
+        public string password1 = "aparnamishra";
+        public void Validate()
+        {
+            Func<string, bool> result = (password1) => Regex.IsMatch(password1, password);
+            Console.WriteLine(result(password1));
+        }
+
+    }
+
+}
