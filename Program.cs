b3ead2a951e96e957c0613b0f313afbbadf03df6
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:20:33 +0530

    Add files via upload
    Adding UC7

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC7
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC7AtLeast1Number obj = new UC7AtLeast1Number();
+            obj.Validate();
+        }
+    }
+}
---
+++ UC7AtLeast1Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC7
+{
+    internal class UC7AtLeast1Number
+    {
+        public string password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+        public string password1 = "aparnaMishra123";
+        public void Validate()
+        {
+            Func<string, bool> result = (password1) => Regex.IsMatch(password1, password);
+            Console.WriteLine(result(password1));
+        }
+    }
+}
