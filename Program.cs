7637c32d51a3db3c7843e661b1d1be9ccad36b1b
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:16:43 +0530

    Add files via upload
    Adding UC4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC4
+{
+    internal class program
+    {
+        public static void Main()
+
+        {
+            UC4MobileNo obj = new UC4MobileNo();
+            obj.Validate();
+        }
+    }
+}
---
+++ UC4MobileNo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC4
+{
+    internal class UC4MobileNo
+    {
+        public string MobileNo = "^[9][1][\\s]?[0-9]{10}$";
+        public string MobileNumber = "91 9955819686";
+        public void Validate()
+        {
+            Func<string, bool> result = (MobileNumber) => Regex.IsMatch(MobileNumber, MobileNo);
+            Console.WriteLine(result(MobileNumber));
+        }
+
+    }
+}
