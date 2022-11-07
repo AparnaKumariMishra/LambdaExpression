502fb63793930bf67cd20debc8767b314fc15d22
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:12:24 +0530

    Add files via upload
    Adding UC2

---
+++ UC2LastName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2
+{
+    internal class UC2LastName
+    {
+        public string LastName = "^[A-Z]{1}[a-z]{2}$";
+        public string LastName1 = "Sah";
+        public void Validate()
+        {
+            Func<string, bool> result = (LastName1) => Regex.IsMatch(LastName1, LastName);
+            Console.WriteLine(result(LastName1));
+        }
+    }
+}
