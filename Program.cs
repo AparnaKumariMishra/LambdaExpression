6efdfb1826f0640c15f1652bf5d2000693711444
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:15:19 +0530

    Add files via upload
    Adding UC3

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC3
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC3Email obj = new UC3Email();
+            obj.Validate();
+
+        }
+    }
+}
---
+++ UC3Email.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC3
+{
+    internal class UC3Email
+    {
+        public string Email = "^[A-Z]{3}[.][a-z]{3}@[a-z]{2}[.][a-z]{2}$";//YAS.raj@co.in
+        public string Email1 = "YAS.raj@co.in";
+        public void Validate()
+        {
+            Func<string, bool> result = (Email1) => Regex.IsMatch(Email1, Email);
+            Console.WriteLine(result(Email1));
+        }
+
+    }
+}
