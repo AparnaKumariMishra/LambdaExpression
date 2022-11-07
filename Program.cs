6756b3026938acb6f743edf50adcf951e99c4ab9
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-07 14:19:06 +0530

    Add files via upload
    Adding UC6

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC6AtLeast1Upercase obj = new UC6AtLeast1Upercase();
+            obj.Validate();
+        }
+    }
+}
---
+++ UC6AtLeast1Upercase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6
+{
+    internal class UC6AtLeast1Upercase
+    {
+        public string password = "^[a-zA-Z]{8,}$";
+        public string password1 = "aparnaMishra";
+        public void Validate()
+        {
+            Func<string, bool> result = (password1) => Regex.IsMatch(password1, password);
+            Console.WriteLine(result(password1));
+        }
+    }
+}
